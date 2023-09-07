7231fe7c4bcce0c0f82206c35765b0e8d93ea995
tboehnel <tom.boehnel@gmail.com>
2023-09-07 11:46:56 -0700

    fix comment

--- utils/sandmining_ml_utils.py
+++ utils/sandmining_ml_utils.py
@@ -20,13 +20,13 @@ SATMEA_NORM_MEAN = np.array([
             1184.3824625 , 1120.77120066, 1136.26026392,
             1263.73947144, 1645.40315151, 1846.87040806, 1762.59530783,
             1972.62420416,  582.72633433,
-            # 14.77112979,  # B10 commenting out because we it's in our .tif EE export
+            # 14.77112979,  # B10 commenting out because we it's not in our .tif EE export
             1732.16362238, 1247.91870117])
 SATMEA_NORM_STD = np.array([
             633.15169573,  650.2842772 ,  712.12507725,  965.23119807,
             948.9819932 , 1108.06650639, 1258.36394548, 1233.1492281,
             1364.38688993,  472.37967789, 
-            #14.3114637 ,  # B10 commenting out because we it's in our .tif EE export
+            #14.3114637 ,  # B10 commenting out because we it's not in our .tif EE export
             1310.36996126, 1087.6020813])
 
 class SatMaeNormS2Transformer(RasterTransformer):
