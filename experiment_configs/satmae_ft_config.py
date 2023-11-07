5536d05078e3276ca86bacd7ebf15519f4e6f0b0
tboehnel <tom.boehnel@gmail.com>
2023-11-06 17:44:09 -0800

    update training configs

--- experiment_configs/satmae_ft_config.py
+++ experiment_configs/satmae_ft_config.py
@@ -12,36 +12,36 @@ satmae_ft_lineardecoder_config = SupervisedFinetuningCofig(
     datasets=DatasetChoice.S2,
     mine_class_loss_weight=2.,
     finetuning_strategy=FinetuningStratagyChoice.LinearProbing,
-    encoder_weights_path=None, # "/data/sand_mining/checkpoints/satmae_orig/pretrain-vit-base-e199.pth"
-    loss_fn=BackpropLossChoice.DICE
+    encoder_weights_path="/data/sand_mining/checkpoints/satmae_orig/pretrain-vit-base-e199.pth"
+    loss_fn=BackpropLossChoice.BCE
 )
 
 satmae_ft_doubleupsampling_config = SupervisedFinetuningCofig(
     model_type=ModelChoice.SatmaeBaseDoubleUpsampling,
     optimizer=OptimizerChoice.AdamW,
-    tile_size=96,
+    tile_size=160,
     s2_channels=satmea_pretrained_encoder_bands_idx,
     s2_normalization=NormalizationS2Choice.ChannelWise,
-    batch_size=2,
+    batch_size=64,
     learning_rate=1e-3,
     datasets=DatasetChoice.S2,
-    mine_class_loss_weight=2.,
-    finetuning_strategy=FinetuningStratagyChoice.FreezeEmbed,
+    mine_class_loss_weight=6.,
+    finetuning_strategy=FinetuningStratagyChoice.LinearProbing,
     encoder_weights_path="/data/sand_mining/checkpoints/satmae_orig/pretrain-vit-base-e199.pth",
-    loss_fn=BackpropLossChoice.DICE
+    loss_fn=BackpropLossChoice.BCE
 )
 
 satmae_large_ft_doubleupsampling_config = SupervisedFinetuningCofig(
     model_type=ModelChoice.SatmaeLargeDoubleUpsampling,
     optimizer=OptimizerChoice.AdamW,
-    tile_size=96,
+    tile_size=160,
     s2_channels=satmea_pretrained_encoder_bands_idx,
     s2_normalization=NormalizationS2Choice.ChannelWise,
     batch_size=64,
-    learning_rate=1e-4,
+    learning_rate=1e-3,
     datasets=DatasetChoice.S2,
-    mine_class_loss_weight=2.,
+    mine_class_loss_weight=6.,
     finetuning_strategy=FinetuningStratagyChoice.LinearProbing,
     encoder_weights_path="/data/sand_mining/checkpoints/satmae_orig/pretrain-vit-large-e199.pth",
-    loss_fn=BackpropLossChoice.DICE
+    loss_fn=BackpropLossChoice.BCE
 )
--- experiment_configs/unet_fs_config.py
+++ experiment_configs/unet_fs_config.py
@@ -3,14 +3,14 @@ from .schemas import *
 unet_orig_config = SupervisedTrainingConfig(
     model_type=ModelChoice.UnetOrig,
     optimizer=OptimizerChoice.AdamW,
-    tile_size=256,
+    tile_size=160,
     s2_channels=None,
     s2_normalization=NormalizationS2Choice.ChannelWise,
-    batch_size=32,
-    learning_rate=1e-3,
+    batch_size=128,
+    learning_rate=5e-4,
     datasets=DatasetChoice.S1S2,
-    mine_class_loss_weight=2.,
-    loss_fn=BackpropLossChoice.DICE
+    mine_class_loss_weight=6.,
+    loss_fn=BackpropLossChoice.BCE
 )
 
 unet_resblocks_config = SupervisedTrainingConfig(
@@ -23,44 +23,5 @@ unet_resblocks_config = SupervisedTrainingConfig(
     learning_rate=1e-3,
     datasets=DatasetChoice.S1S2,
     mine_class_loss_weight=2.,
-    loss_fn=BackpropLossChoice.DICE
-)
-
-resnet18_unet_config = SupervisedTrainingConfig(
-    model_type=ModelChoice.ResNet18UNet,
-    optimizer=OptimizerChoice.AdamW,
-    tile_size=256,
-    s2_channels=None,
-    s2_normalization=NormalizationS2Choice.ChannelWise,
-    batch_size=64,
-    learning_rate=1e-3,
-    datasets=DatasetChoice.S1S2,
-    mine_class_loss_weight=2.,
-    loss_fn=BackpropLossChoice.DICE
-)
-
-resnet34_unet_config = SupervisedTrainingConfig(
-    model_type=ModelChoice.ResNet34UNet,
-    optimizer=OptimizerChoice.AdamW,
-    tile_size=256,
-    s2_channels=None,
-    s2_normalization=NormalizationS2Choice.ChannelWise,
-    batch_size=128,
-    learning_rate=1e-3,
-    datasets=DatasetChoice.S1S2,
-    mine_class_loss_weight=2.,
-    loss_fn=BackpropLossChoice.DICE
-)
-
-resnet50_unet_config = SupervisedTrainingConfig(
-    model_type=ModelChoice.ResNet50UNet,
-    optimizer=OptimizerChoice.AdamW,
-    tile_size=256,
-    s2_channels=None,
-    s2_normalization=NormalizationS2Choice.ChannelWise,
-    batch_size=128,
-    learning_rate=5e-4,
-    datasets=DatasetChoice.S1S2,
-    mine_class_loss_weight=2.,
-    loss_fn=BackpropLossChoice.DICE
+    loss_fn=BackpropLossChoice.BCE
 )
