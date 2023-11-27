95aacf4d4c72bab31474513788408e1c64c63c7e
Ando Shah <ando@berkeley.edu>
2023-11-26 16:55:13 -0800

    Updated dataset with automated clustering

--- project_config.py
+++ project_config.py
@@ -63,7 +63,7 @@ RIVER_BUFFER_M = '1000m'
 # find the file path by looking for the latest version and date in the file name
 # e.g. annotations_json_v0.1_2021-04-08.json
 
-DATASET_JSON_PATH = 'dataset/dataset_v0.1.0_2023-11-16.json'
+DATASET_JSON_PATH = 'dataset/dataset_v0.2_2023-11-26.json'
 
 RIVER_NETWORKS_DIR = '/data/sand_mining/rivers/'
 
