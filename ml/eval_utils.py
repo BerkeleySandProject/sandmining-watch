Checking gradient flow and fixing loss calculation after forward pass Satlas

ml/eval_utils.py
@@ -1,3 +1,6 @@
+import os
+import rasterio
+from rasterio.transform import from_origin
 import numpy as np
 import pandas as pd
 from sklearn.metrics import average_precision_score, precision_recall_curve
@@ -19,15 +22,21 @@ def evaluate_predictions(prediction_results):
 
     eval_dict = {}
     for prediction_result_dict in prediction_results:
-        # We discard the edge pixels in each observation. This ensures that 
+        # We discard the edge pixels in each observation. This ensures that
         # results are comparable, even if the window size is different.
         # (But N_EDGE_PIXELS_DISCARD needs to remain consistent)
-        
-        prediction = center_crop(prediction_result_dict['predictions'], prediction_result_dict['crop_sz']).ravel()
-        gt = center_crop(prediction_result_dict['ground_truth'], prediction_result_dict['crop_sz']).ravel()
-        observation_name = prediction_result_dict['name']
-        #also crop the rgb image so that the dimensions match with predictions and gt
-        prediction_result_dict['rgb_img'] = center_crop(prediction_result_dict['rgb_img'], prediction_result_dict['crop_sz'])
+
+        prediction = center_crop(
+            prediction_result_dict["predictions"], prediction_result_dict["crop_sz"]
+        ).ravel()
+        gt = center_crop(
+            prediction_result_dict["ground_truth"], prediction_result_dict["crop_sz"]
+        ).ravel()
+        observation_name = prediction_result_dict["name"]
+        # also crop the rgb image so that the dimensions match with predictions and gt
+        prediction_result_dict["rgb_img"] = center_crop(
+            prediction_result_dict["rgb_img"], prediction_result_dict["crop_sz"]
+        )
 
         if ANNO_CONFIG.num_classes == 3:
             mask = gt != 1
@@ -46,17 +55,26 @@ def evaluate_predictions(prediction_results):
             # because we will have no true positives.
             continue
 
-        precision, recall, f1_score, average_precision, best_threshold, best_f1_score = compute_metrics(gt, prediction)
+        (
+            precision,
+            recall,
+            f1_score,
+            average_precision,
+            best_threshold,
+            best_f1_score,
+        ) = compute_metrics(gt, prediction)
         best_thresholds.append(best_threshold)
 
-        eval_dict.update({
-            f"eval/{observation_name}/precision": precision,
-            f"eval/{observation_name}/recall": recall,
-            f"eval/{observation_name}/f1_score": f1_score,
-            f"eval/{observation_name}/average_precision": average_precision,
-            f"eval/{observation_name}/best_threshold": best_threshold,
-            f"eval/{observation_name}/best_f1_score": best_f1_score,
-        })
+        eval_dict.update(
+            {
+                f"eval/{observation_name}/precision": precision,
+                f"eval/{observation_name}/recall": recall,
+                f"eval/{observation_name}/f1_score": f1_score,
+                f"eval/{observation_name}/average_precision": average_precision,
+                f"eval/{observation_name}/best_threshold": best_threshold,
+                f"eval/{observation_name}/best_f1_score": best_f1_score,
+            }
+        )
 
     all_predictions = np.concatenate(all_predictions_list)
     all_gt = np.concatenate(all_gt_list)
@@ -65,27 +83,37 @@ def evaluate_predictions(prediction_results):
     median_threshold = np.median(best_thresholds)
     sd_threshold = np.std(best_thresholds)
 
-    precision, recall, f1_score, average_precision, _, _,  = compute_metrics(all_gt, all_predictions)
-    eval_dict.update({
-        f"eval/total/precision": precision,
-        f"eval/total/recall": recall,
-        f"eval/total/f1_score": f1_score,
-        f"eval/total/average_precision": average_precision,
-        f"eval/total/mean_threshold": mean_threshold,
-        f"eval/total/median_threshold": median_threshold,
-        f"eval/total/sd_threshold": sd_threshold,
-    })
+    (
+        precision,
+        recall,
+        f1_score,
+        average_precision,
+        _,
+        _,
+    ) = compute_metrics(all_gt, all_predictions)
+    eval_dict.update(
+        {
+            f"eval/total/precision": precision,
+            f"eval/total/recall": recall,
+            f"eval/total/f1_score": f1_score,
+            f"eval/total/average_precision": average_precision,
+            f"eval/total/mean_threshold": mean_threshold,
+            f"eval/total/median_threshold": median_threshold,
+            f"eval/total/sd_threshold": sd_threshold,
+        }
+    )
 
-    #replace nans in all_gt and all_predictions with 0
+    # replace nans in all_gt and all_predictions with 0
     all_gt = np.nan_to_num(all_gt)
     all_predictions = np.nan_to_num(all_predictions)
 
-
     if wandb.run is not None:
         pr_curve = make_precision_recall_curve_plot(all_gt, all_predictions)
-        eval_dict.update({
-            f"eval/total/precision_recall_curve": pr_curve,
-        })
+        eval_dict.update(
+            {
+                f"eval/total/precision_recall_curve": pr_curve,
+            }
+        )
     return eval_dict
 
 
@@ -94,8 +122,9 @@ def make_precision_recall_curve_plot(gt, predictions):
     # -> hack: we duplicate the array of shape (n,) into (n,2)
     preds = np.stack([predictions, predictions], axis=1)
     return wandb.plot.pr_curve(
-        gt, preds, classes_to_plot=[1], labels=['other', 'sandmine']
+        gt, preds, classes_to_plot=[1], labels=["other", "sandmine"]
     )
+
 
 def make_wandb_segmentation_masks(prediction_results, threshold=0.5):
     """
@@ -104,14 +133,17 @@ def make_wandb_segmentation_masks(prediction_results, threshold=0.5):
     Expects prediction_results to be a list of dictionaries. Each dictonary is expected
     to have the keys 'predictions', 'ground_truth', 'rgb_img' and 'name'.
     """
+
     def prediction_results_to_wandb_mask(input):
         return create_semantic_segmentation_image(
-            background_image=center_crop(input['rgb_img']),
-            predicted_mask=center_crop(input['predictions']) >= threshold,
-            ground_truth_mask=center_crop(input['ground_truth']),
-            image_title=input['name']
-        )    
+            background_image=center_crop(input["rgb_img"]),
+            predicted_mask=center_crop(input["predictions"]) >= threshold,
+            ground_truth_mask=center_crop(input["ground_truth"]),
+            image_title=input["name"],
+        )
+
     return list(map(prediction_results_to_wandb_mask, prediction_results))
+
 
 def make_wandb_predicted_probs_images(prediction_results):
     """
@@ -120,23 +152,29 @@ def make_wandb_predicted_probs_images(prediction_results):
     Expects prediction_results to be a list of dictionaries. Each dictonary is expected
     to have the keys 'predictions', 'ground_truth', 'rgb_img' and 'name'.
     """
+
     def create_predicted_probabilities_image(input):
-        return wandb.Image(
-            center_crop(input['predictions']),
-            caption=input['name']
-        )
+        return wandb.Image(center_crop(input["predictions"]), caption=input["name"])
+
     return list(map(create_predicted_probabilities_image, prediction_results))
 
+
 def center_crop(array, n_crop_pixels=0):
-# def center_crop(array, n_crop_pixels=N_EDGE_PIXELS_DISCARD):
+    # def center_crop(array, n_crop_pixels=N_EDGE_PIXELS_DISCARD):
     """
     Given a 2-dimensional array, crops this array by n_crop_pixels at each edge
     """
-    return array[n_crop_pixels:-n_crop_pixels, n_crop_pixels:-n_crop_pixels] if n_crop_pixels > 0 else array
+    return (
+        array[n_crop_pixels:-n_crop_pixels, n_crop_pixels:-n_crop_pixels]
+        if n_crop_pixels > 0
+        else array
+    )
+
 
 def compute_metrics(ground_truth, predicted_prob, predicted_class=None):
     eps = 1e-6
-    valid_mask = ~np.isnan(predicted_prob)  # Create a mask for valid (non-nan) values
+    # Create a mask for valid (non-nan) values
+    valid_mask = ~np.isnan(predicted_prob)
 
     # print("Number of valid pixels: ",valid_mask.sum())
 
@@ -158,24 +196,32 @@ def compute_metrics(ground_truth, predicted_prob, predicted_class=None):
     recall = TP / (TP + FN + eps)
     f1_score = 2 * (precision * recall) / (precision + recall)
 
-    precisions_t, recalls_t, thresholds = precision_recall_curve(valid_ground_truth, valid_predicted_prob)
+    precisions_t, recalls_t, thresholds = precision_recall_curve(
+        valid_ground_truth, valid_predicted_prob
+    )
     f1_scores_t = 2 * (precisions_t * recalls_t) / (precisions_t + recalls_t + eps)
 
-    average_precision = average_precision_score(valid_ground_truth, valid_predicted_prob)
+    average_precision = average_precision_score(
+        valid_ground_truth, valid_predicted_prob
+    )
 
-    #get best threshold
+    # get best threshold
     ix = np.argmax(f1_scores_t)
-    return precision, recall, f1_score, average_precision, thresholds[ix], f1_scores_t[ix]
+    return (
+        precision,
+        recall,
+        f1_score,
+        average_precision,
+        thresholds[ix],
+        f1_scores_t[ix],
+    )
 
-
-import rasterio, os
-from rasterio.transform import from_origin
 
 def save_mask_as_geotiff(mask, transform, crs, filename):
     with rasterio.open(
         filename,
-        'w',
-        driver='GTiff',
+        "w",
+        driver="GTiff",
         height=mask.shape[0],
         width=mask.shape[1],
         count=1,
@@ -185,19 +231,20 @@ def save_mask_as_geotiff(mask, transform, crs, filename):
     ) as dst:
         dst.write(mask, 1)
 
-def save_predictions(
-        prediction,
+
+def save_predictions(prediction, path, class_config, crs_transformer, threshold=0.5):
+    prediction.save(
         path,
-        class_config,
-        crs_transformer,
-        threshold=0.5
-    ):
-    
-    prediction.save(path, class_config=class_config, crs_transformer=crs_transformer, discrete_output=False)
+        class_config=class_config,
+        crs_transformer=crs_transformer,
+        discrete_output=False,
+    )
 
     prediction_score = prediction.get_score_arr(prediction.extent)[0]
 
     mask = np.where(prediction_score >= threshold, 1, 0)
 
-    mask_filename = os.path.join(path, 'mask.tif')
-    save_mask_as_geotiff(mask, crs_transformer.transform, crs='EPSG:4326', filename=mask_filename)
+    mask_filename = os.path.join(path, "mask.tif")
+    save_mask_as_geotiff(
+        mask, crs_transformer.transform, crs="EPSG:4326", filename=mask_filename
+    )

ml/learner.py
@@ -274,9 +274,11 @@ class Learner(ABC):
         # x, y, w = batch
         x, y = batch
         out = self.post_forward(self.model(x))
+        out_classes = torch.softmax(out, dim=1).float()
+
         # In the following, we hardcoded our metric names for our loss functions
 
-        return self.calculate_losses(out, y, "train_")
+        return self.calculate_losses(out_classes, y, "train_")
 
     def calculate_losses(self, out, y, prefix=""):
         return {

ml/optimizer_factory.py
@@ -1,12 +1,22 @@
 from torch import nn
 import torch.optim as optim
-#from models.satmae.util.lr_decay import param_groups_lrd_2
 
+# from models.satmae.util.lr_decay import param_groups_lrd_2
+import ipdb
 from experiment_configs.schemas import SupervisedTrainingConfig, OptimizerChoice
 
-def optimizer_factory(config:SupervisedTrainingConfig , model:nn.Module) -> optim.Optimizer:
+
+def optimizer_factory(
+    config: SupervisedTrainingConfig, model: nn.Module
+) -> optim.Optimizer:
     if config.optimizer == OptimizerChoice.AdamW:
-        return optim.AdamW(model.parameters(), lr=config.learning_rate)
+        ipdb.set_trace()
+        # DEBUG: Check that optimizer only optimzes certain specified layers
+        # return optim.AdamW(model.parameters(), lr=config.learning_rate)
+        return optim.AdamW(
+            filter(lambda p: p.requires_grad is True, model.parameters()),
+            lr=config.learning_rate,
+        )
     elif config.optimizer == OptimizerChoice.SDG:
         return optim.SGD(model.parameters(), lr=config.learning_rate)
     else:

models/satlas/satlas_pretrained_encoder.py
@@ -79,6 +79,13 @@ class SatlasPretrained(torch.nn.Module):
         else:
             self.head = None
 
+        ipdb.set_trace()
+        # DEBUG: Ensure that all Upsample and Head param groups require gradients
+        for param in self.upsample.parameters():
+            param.requires_grad = True
+        for param in self.head.parameters():
+            param.requires_grad = True
+
     def load_encoder_weights(self, path_to_weights):
         if not os.path.isfile(path_to_weights):
             raise ValueError(f"No checkpoint found at {path_to_weights}")
@@ -105,6 +112,11 @@ class SatlasPretrained(torch.nn.Module):
     def freeze_encoder_weights(self):
         print("SatlasPretrained: Freezing backbone/encoder weights")
         for param in self.backbone.parameters():
+            param.requires_grad = False
+
+    def freeze_fpn_weights(self):
+        print("SatlasPretrained: Freezing FPN weights")
+        for param in self.fpn.parameters():
             param.requires_grad = False
 
     def _initialize_backbone(
@@ -181,20 +193,11 @@ class SatlasPretrained(torch.nn.Module):
 
         ipdb.set_trace()
         # DEBUG: Ensure forward pass works properly
-        # if self.fpn == True:
-        #     x = self.fpn(x)
-        #     x = self.upsample(x)
-        # if self.head:
-        #     x, loss = self.head(imgs, x, targets)
-        #     reshaped_tensor = x.view(32, 32, 3, 40, 40)
-        #     upsampled_tensor = F.interpolate(
-        #         reshaped_tensor, size=(160, 160), mode="bilinear", align_corners=False
-        #     )
-        #     segmentation_output = torch.argmax(upsampled_tensor, dim=1)
-        #     return segmentation_output, loss
-        x = self.backbone(x)
-        x = self.upsample(x)
-        x = self.head(raw_features=x)
-        x = torch.argmax(x[0], dim=1).unsqueeze(1).float()
+        with torch.no_grad():
+            x1 = self.backbone(x)
+            x2 = self.fpn(x1)
+        x3 = self.upsample(x2)
+        x4 = self.head(raw_features=x3)[0]
+        # x = torch.argmax(x[0], dim=1).unsqueeze(1).float()
 
-        return x
+        return x4